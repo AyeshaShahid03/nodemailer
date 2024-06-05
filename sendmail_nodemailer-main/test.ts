e8406b62d8f0ded08c73a11b702de491ea8ee20d
Ayesha Shahid <ayeshaashii20g@gmail.com>
2024-06-05 12:12:56 +0500

    troubleshoot: test.ts

--- sendmail_nodemailer-main/test.ts
+++ sendmail_nodemailer-main/test.ts
@@ -1,14 +1,12 @@
-import { Request, Response, NextFunction } from "express";
-import { userModel } from "_app/models";
-import * as fs from "fs";
+import * as express from 'express';
+import bodyParser from "body-parser";
+import nodemailer from "nodemailer";
+import { google } from "googleapis";
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const nodemailer = require("nodemailer");
-const { google } = require("googleapis");
 const OAuth2 = google.auth.OAuth2;
+
 const PORT = process.env.PORT || 3000;
-const app = express();
+const app = express()as express.Application;;
 
 const myOAuth2Client = new OAuth2(
   "758091215662-a8quu3phbis1t3em84hc4er2djb36ubn.apps.googleusercontent.com",
@@ -20,50 +18,64 @@ myOAuth2Client.setCredentials({
   refresh_token:
     "1//04OxJEPLjhKCtCgYIARAAGAQSNwF-L9IrzlrGt3gIHV6M_QPNvkB5wO9VzdGCWs1wo0kVt-66o0jFR4Lc7ZGNZWqhPzUZbEdw_ug",
 });
-const myAccessToken = myOAuth2Client.getAccessToken();
 
 const transport = nodemailer.createTransport({
   service: "gmail",
   auth: {
     type: "OAuth2",
-    user: "support@wafflestock.com", //your gmail account you used to set the project up in google cloud console"
+    user: "support@wafflestock.com",
     clientId: "758091215662-a8quu3phbis1t3em84hc4er2djb36ubn.apps.googleusercontent.com",
     clientSecret: "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",
     refreshToken:
       "1//04OxJEPLjhKCtCgYIARAAGAQSNwF-L9IrzlrGt3gIHV6M_QPNvkB5wO9VzdGCWs1wo0kVt-66o0jFR4Lc7ZGNZWqhPzUZbEdw_ug",
-    accessToken: myAccessToken, //access token variable we defined earlier
+    accessToken: myOAuth2Client.getAccessToken(),
   },
 });
 
 app.use(bodyParser.json());
 
-export class MailerService {
-  static _mailsInstance: MailerService = null;
+class MailerService {
+  private static _mailInstance: MailerService | null = null;
 
-  static getInstance() {
-    if (!MailerService._mailsInstance) {
-      MailerService._mailsInstance = new MailerService();
+  private constructor() {}
+
+  static getInstance(): MailerService {
+    if (!MailerService._mailInstance) {
+      MailerService._mailInstance = new MailerService();
     }
-    return MailerService._mailsInstance;
+    return MailerService._mailInstance as MailerService;
   }
 
-  public async sendEmail(receiverEmail: String, subject: String, html: String) {
-    //const html = fs.readFileSync(__dirname + "/../emailTemplates/verify-email.html", "utf8").toString();
-    var mailOptions = {
-      from: "<Support> support@wafflestock.com", // sender
-      to: receiverEmail, // receiver
-      subject: subject, // Subject
-      // html: '<p>You have received this email using nodemailer, you are welcome123 ;)</p>'// html body
-      html: html, //'<p>You have received this email using nodemailer, you are welcome123 ;)</p>'
+  public async sendEmail(receiverEmail: string, subject: string, html: string) {
+    const mailOptions = {
+      from: "<Support> support@wafflestock.com",
+      to: receiverEmail,
+      subject: subject,
+      html: html,
     };
 
-    transport.sendMail(mailOptions, function (err: any, result: any) {
-      if (err) {
-        return err;
-      } else {
-        transport.close();
-        return result;
-      }
-    });
-  }
+    try {
+      const result = await transport.sendMail(mailOptions);
+      console.log("Email sent successfully:", result);
+      return result;
+    } catch (error) {
+      console.error("Error sending email:", error);
+      throw error;
+    }
+  }
 }
+
+app.post("/send-email", async (req, res) => {
+  const { receiverEmail, subject, html } = req.body;
+
+  try {
+    await MailerService.getInstance().sendEmail(receiverEmail, subject, html);
+    res.status(200).send("Email sent successfully");
+  } catch (error) {
+    res.status(500).send("Error sending email");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
